ece2374dbbd2a67132158c65c2c9f59b522a6d8b
CindyMisoi <crmisoi2k@gmail.com>
2023-01-12 21:38:38 +0300

    partly completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(user) {
+ 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <p>{user.bio}</p>
+      {/* conditional rendering */}
+     
+      <Links/>
     </div>
   );
+  
 }
 
 export default About;
